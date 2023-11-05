7c3d3e401853abfd1f2eb62f17d606edcd92f158
yuasalily <yuasalily1011@gmail.com>
2023-11-05 16:30:19 +0000

    生成用sh作成

---
+++ generate.sh
@@ -0,0 +1,24 @@
+merge() {
+    # commonとそれぞれの言語用設定ファイルを上書きor結合
+    for f in $(ls common); do
+        cat common/$f $1/$f | jq -s add > tmp_$f
+    done
+
+    # vscodeを生成
+    cat tmp_extensions.json tmp_settings.json | jq -s '{vscode:{extensions: .[0], settings: .[1]}}' > tmp_vscode.json
+    rm -f tmp_extensions.json tmp_settings.json
+
+    # customizationsを生成
+    cat tmp_vscode.json | jq -s '{customizations:.[0]}' > tmp_customizations.json
+    rm -f tmp_vscode.json
+
+    # devcontainerの設定ファイル生成
+    cat tmp_* | jq -s add > $1.json
+
+}
+
+# todo: common以外のfolderをforで回すようにする
+merge go
+merge rust
+
+rm -f tmp_*
